8e59a442e6d6fd9ef45d99172b37fa8510f0baaa
Patrick Widjaja <patrickwidjaja@patricks-macbook-pro.local>
2018-12-26 10:53:04 +0700

    Initial Commit

---
+++ FlashlightTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashlightTest
+//
+//  Created by Patrick Widjaja on 26/12/18.
+//  Copyright © 2018 Patrick Widjaja Organisation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
