dfca4f9ae93ea5b7e81a3404d79c0b09e3cf2538
Patrick Widjaja <patrickwidjaja@patricks-macbook-pro.local>
2018-12-29 08:50:53 +0700

    Added functional flashlight.

--- FlashlightTest/ViewController.swift
+++ FlashlightTest/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onOff(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Off" {
+            sender.setTitle("On", for: .normal)
+            view.backgroundColor = .black
+        } else if sender.titleLabel?.text == "On" {
+            sender.setTitle("Off", for: .normal)
+            view.backgroundColor = .white
+        }
+    }
+    
 }
 
